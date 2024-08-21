bd4a97c12a811c2adbb4177f525a0821ac2104fb
Dawam <muhammadawamali@gmail.com>
2024-08-21 19:24:47 +0700

    yaudah lyah

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/home"
+import Navbar from "./components/blocks/Navbar"
+import Footer from "./components/blocks/Footer"
+
+//pages
+import Home from "./pages/Home"
 
 export default function App() {
   return (
     <BrowserRouter>
+
+    <Navbar />
+
     <Routes>
-      <Route path="/"
-      element={<Home />} />
+      <Route path="/" element={<Home />} />
     </Routes>
+
+    <Footer />
     </BrowserRouter>
 
   )
